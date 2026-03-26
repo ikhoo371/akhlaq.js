5779d81cb2db68410197d03b3c436e592285b2cf
akhlaq ahmed <akhlaqc91@gmail.com>
2026-03-26 23:33:37 +0200

    operation

--- basic1/operation.js
+++ basic1/operation.js
@@ -5,3 +5,27 @@ let nevalue = -value
 console.log(2+2)
 console.log(2+2)
 console.log(2+2)*/
+
+//datatypes
+
+//primitive
+
+// 7 types : string , num , bool , null ,undefined
+//symbol,bigint
+
+// Non primitive
+
+// array, objects , functions
+
+
+// array will be in square brackets
+//objects will in the curly brackets
+ const heros = [me ,you , he];
+ let myobj = {
+    myname : "ikhoo",
+    age: 19,
+ }
+
+ const myfunctions = function(){
+    console.log("ikhoo"));
+ }
