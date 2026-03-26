792abd30e1275fd62a4dfe17e0db717a54a1f32d
akhlaq ahmed <akhlaqc91@gmail.com>
2026-03-26 23:59:58 +0200

    new

---
+++ basic1/strings.js
@@ -0,0 +1,6 @@
+const name = "ikhoo"
+const repocount = 50
+ // concatinations exaples
+ //console.log(name + repocount + "value")
+ console.log(`hello mu name is ${name}
+     and my repocount is ${repocount}`);
