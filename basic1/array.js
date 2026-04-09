30e1f8464824e3ed353562bd2e01a03b7308ee7d
akhlaq ahmed <akhlaqc91@gmail.com>
2026-04-10 00:26:23 +0300

    new

---
+++ basic1/array.js
@@ -0,0 +1,42 @@
+const Arr = [0, 1, 2, 3, 4, 5];
+
+//console.log(Arr[2]);
+
+const heros = [ "ikhoo" , "me"]
+
+const arr2 = new Array(1 , 2 , 3 , 4)
+
+//console.log(Arr[1]);
+//console.log(heros[1]);
+
+//array method
+
+//Arr.push(4)  //adding value to array
+//console.log(Arr);
+
+//Arr.pop()  // remove last value from array 
+//console.log(Arr)
+ 
+//Arr.unshift(9)
+//console.log(Arr)  // adding value to first of array
+
+// using shift will remove first value 
+// shift and pop are without arguments
+
+//console.log(Arr.includes(5)) // asking to check
+//  if there is a value 5 in array 
+
+//console.log(Arr.indexOf(4)) // checking index
+
+const arr12 = Arr.join()
+
+console.log(Arr)
+console.log(typeof Arr12)
+// join change type to another one
+
+//slice , splice
+
+console.log("a" , Arr)
+const myn = Arr.slice(1,3)
+console.log(myn)
+console.log("b" , Arr)
---
+++ basic2/control.js
@@ -0,0 +1,7 @@
+const temp = 40
+
+if(temp == 50){
+    console.log("temp is less then 50")
+}else{
+    console.log("temp is greater then 50")
+}
